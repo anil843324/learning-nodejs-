d92240a6dabc484d039559e8a7ac362adb19c74b
anil843324 <anilkrsingh14200@gmail.com>
2022-09-11 20:35:27 +0530

    routing

--- OwnComplete-node-bootcamp/1-node-farm/starter/index.js
+++ OwnComplete-node-bootcamp/1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs"); //function for reading data and writing data right to the file system
 const http=require('http');
-
+const url=require('url');
 
 
 
@@ -48,8 +48,22 @@ const http=require('http');
 // SERVER
 
  const server=http.createServer((req,res)=>{
-     
-     res.end('hello from the server!');
+   
+     const pathName= req.url;
+      
+     if( pathName==='/' || pathName==='/overview'){
+          res.end('This is the OVERVIEW!');
+     }else if(pathName==='/product'){
+          res.end('This is the PRODUCT!');
+     }else{
+
+           res.writeHead(404,{
+               'Content-type' :'text/html',
+               'my-own-header':'hello-world'
+           });
+          res.end('<h1>  Page  not found!</h1>');
+     }
+    
 
 })
 
