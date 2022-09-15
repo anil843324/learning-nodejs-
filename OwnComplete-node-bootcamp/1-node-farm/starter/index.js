8a99a973c191f13198dc7f20f96a7c90f7d98a1f
anil843324 <anilkrsingh14200@gmail.com>
2022-09-15 22:42:34 +0530

    card and overview done

--- OwnComplete-node-bootcamp/1-node-farm/starter/index.js
+++ OwnComplete-node-bootcamp/1-node-farm/starter/index.js
@@ -47,23 +47,59 @@ const url=require('url');
 ///////////////////////////////////////////////////////////////////////////////////
 // SERVER
 
-// 
+  const replaceTemplate=(temp,product)=>{
+
+      let output=temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+
+      output=output.replace(/{%IMAGE%}/g, product.image)
+      output=output.replace(/{%PRICE%}/g, product.price)
+      output=output.replace(/{%QUANTITY%}/g, product.quantity)
+      output=output.replace(/{%FROM%}/g, product.from)
+      output=output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+      output=output.replace(/{%DESCRIPTION%}/g, product.description)
+      output=output.replace(/{%ID%}/g, product.id)
+
+      // orgqanic
+       if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+       return output
+  }
+
+
+const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+
+const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+
+const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
  const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
+
      const  dataObj= JSON.parse(data);
 
  const server=http.createServer((req,res)=>{
    
      const pathName= req.url;
-      
+      // Overview page
      if( pathName==='/' || pathName==='/overview'){
-          res.end('This is the OVERVIEW!');
+
+          res.writeHead(200,{'Content-type':'text/html' });
+
+          // tempCard
+      const cardsHtml= dataObj.map(ele=>  replaceTemplate(tempCard,ele)).join('');
+
+   const  output=tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+       
+          res.end(output);
+          // product page
      }else if(pathName==='/product'){
           res.end('This is the PRODUCT!');
+          // api
      }else if(pathName==='/api'){
          
           res.writeHead(200,{'Content-type':'application/json' });
           res.end(data);
          
+          // not found
      }else{
 
            res.writeHead(404,{
