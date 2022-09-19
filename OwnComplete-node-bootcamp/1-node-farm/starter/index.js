f96167f95d8551acf6a90c473f3b1ca4a02cf453
anil843324 <anilkrsingh14200@gmail.com>
2022-09-19 16:20:05 +0530

    types of pages and installs

--- OwnComplete-node-bootcamp/1-node-farm/starter/index.js
+++ OwnComplete-node-bootcamp/1-node-farm/starter/index.js
@@ -3,6 +3,10 @@ const http=require('http');
 const url=require('url');
 
 
+// own moduel
+
+ const replaceTemplate= require('./modules/replaceTemplate')
+
 
 ///////////////////////////////////////////////////////////////////////////////////
 // FILES
@@ -47,24 +51,6 @@ const url=require('url');
 ///////////////////////////////////////////////////////////////////////////////////
 // SERVER
 
-  const replaceTemplate=(temp,product)=>{
-
-      let output=temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-
-      output=output.replace(/{%IMAGE%}/g, product.image)
-      output=output.replace(/{%PRICE%}/g, product.price)
-      output=output.replace(/{%QUANTITY%}/g, product.quantity)
-      output=output.replace(/{%FROM%}/g, product.from)
-      output=output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-      output=output.replace(/{%DESCRIPTION%}/g, product.description)
-      output=output.replace(/{%ID%}/g, product.id)
-
-      // orgqanic
-       if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-       return output
-  }
-
 
 const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 
@@ -88,6 +74,7 @@ const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`
 
           res.writeHead(200,{'Content-type':'text/html' });
 
+          
           // tempCard
       const cardsHtml= dataObj.map(ele=>  replaceTemplate(tempCard,ele)).join('');
 
