ff6b57c23c91d839621297a44c77d71ef21570e3
anil843324 <anilkrsingh14200@gmail.com>
2022-09-23 09:40:12 +0530

    build promises

--- OwnComplete-node-bootcamp/3-asynchronous-JS/starter/index.js
+++ OwnComplete-node-bootcamp/3-asynchronous-JS/starter/index.js
@@ -1,24 +1,47 @@
 const fs = require('fs');
 
 const superagent = require('superagent');
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+
+ const  readFilePro= file=>{
+ 
+    return new Promise( (resolve,reject)=>{
+      fs.readFile(file,(err,data)=>{
+         if(err) reject('I could not find that file 😔')
+         resolve(data);
+      })
+    })
+
+ }
+
+ // how to create promise
+
+  const writeFilePro=(file,data)=>{
+
+      return new Promise((resolve,reject)=>{
+          fs.writeFile(file,data, err=>{
+
+            if(err)  reject('Cloud no wirte file 😢')
+               resolve('success')
+          })
+      })
+  }
+
+ readFilePro(`${__dirname}/dog.txt`).then((data)=>{
   console.log(`Breed: ${data}`);
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res=>{
+  return superagent
+    
+  .get(`https://dog.ceo/api/breed/${data}/images/random`)
+ }).then(res=>{
       console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-         if(err) return console.log(err.message)
-        console.log('Random do image save to file');
-      });
+       return writeFilePro('dog-img.txt',res.body.message)
+    })
+    .then(()=>{
+      console.log('Random do image save to file');
     })
     .catch(err=>{
        console.log(err.message);
     })
    
-     
 
-    
-});
+
