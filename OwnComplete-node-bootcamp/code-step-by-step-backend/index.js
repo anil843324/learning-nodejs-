55ddc5181dd07e5ffe238837635654104d8dd0c9
anil843324 <anilkrsingh14200@gmail.com>
2022-09-30 20:44:08 +0530

    render json data and tittle html

--- OwnComplete-node-bootcamp/code-step-by-step-backend/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/index.js
@@ -3,15 +3,42 @@ const  express=require('express')
 const app=express();
 
 app.get('',(req,res)=>{
-
-     console.log('Data sent by brower =>>>>',req.query.name)
-     
-    res.send('Welcome to Home ' +req.query.name);
+  
+    res.send(` 
+    <h1> Welcome , to Home page </h1>
+    <a href="/about">Go to about page</a>
+    `);
 })
 
 app.get('/about',(req,res)=>{
 
-    res.send('Hello , this is About page');
+    res.send(`
+    <input type="text"  placeholder="User Name" value="${req.query.name}">
+    <button>
+    Click Me
+   </button>
+   <a href="/">Go to Home page</a>
+
+    `);
+})
+
+
+app.get("/help",(req,res)=>{
+     
+    res.send([
+        {
+            name :'anil',
+            email:"anilkrsingh142200@gmail.com"
+       },
+       {
+        name :'sunil',
+        email:"sunilkrsingh142200@gmail.com"
+   },
+   {
+    name :'Ratan',
+    email:"ratan0@gmail.com"
+}
+    ])
 })
 
 
