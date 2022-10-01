38d972a11afed2bb128b64bf4d53dc0c110c530f
anil843324 <anilkrsingh14200@gmail.com>
2022-10-01 21:48:31 +0530

    sussefully connected with mongodb 😁

--- OwnComplete-node-bootcamp/code-step-by-step-backend/middleware-with-express/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/middleware-with-express/index.js
@@ -1,44 +1,67 @@
-const express=require('express');
+const { MongoClient } = require("mongodb");
 
- const reqFilter=require('./middleware')
- const app=express();
+const url = "mongodb://localhost:27017";
 
-const route=express.Router()
+const database = "e-commerce";
+
+const clinet = new MongoClient(url);
+
+async function getData() {
+
+  let result = await clinet.connect();
+
+  let db = result.db(database);
+
+  let collection = db.collection("products");
+
+  let response = await collection.find({}).toArray();
+
+  console.log(response);
+}
+
+getData();
+
+
+
 
 // middleware
+// const express=require('express');
 
+//  const reqFilter=require('./middleware')
+//  const app=express();
 
-// app.use(reqFilter)
+// const route=express.Router()
 
-route.use(reqFilter)
+// // middleware
 
- app.get( '',(req,res)=>{
+// // app.use(reqFilter)
 
-     res.send('Welcome to Home Page')
+// route.use(reqFilter)
 
- })
+//  app.get( '',(req,res)=>{
 
+//      res.send('Welcome to Home Page')
 
- 
- app.get( '/users' ,(req,res)=>{
+//  })
 
-    res.send('Welcome to Users Page')
+//  app.get( '/users' ,(req,res)=>{
 
-})
+//     res.send('Welcome to Users Page')
 
-route.get( '/about', (req,res)=>{
+// })
 
-   res.send('Welcome to About Page')
+// route.get( '/about', (req,res)=>{
 
-})
+//    res.send('Welcome to About Page')
 
-route.get( '/contact',(req,res)=>{
+// })
 
-   res.send('Welcome to Contact Page')
+// route.get( '/contact',(req,res)=>{
 
-})
+//    res.send('Welcome to Contact Page')
 
-app.use('/',route)
+// })
 
-app.listen(8000)
+// app.use('/',route)
 
+// app.listen(8000)
