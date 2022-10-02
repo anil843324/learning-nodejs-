cc7107a75ab36a75e6881b5dc96927972fb84f7d
anil843324 <anilkrsingh14200@gmail.com>
2022-10-02 09:20:44 +0530

    post method with mongodb 😁

--- OwnComplete-node-bootcamp/code-step-by-step-backend/middleware-with-express/api.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/middleware-with-express/api.js
@@ -4,10 +4,27 @@ const dbConnect = require("./mongodb");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", async (req, res) => {
   let data = await dbConnect();
   data = await data.find().toArray();
   res.send(data);
+
 });
+
+app.post('/', async (req,res)=>{
+
+
+
+    console.log(req.body);
+
+     let data=await dbConnect()
+
+    let result= await data.insert(req.body)
+
+   res.send(result)
+
+})
 
 app.listen(5000);
