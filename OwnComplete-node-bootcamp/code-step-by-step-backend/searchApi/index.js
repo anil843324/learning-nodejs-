fb90992ddcba852d185f5ef5c510a8c4fb0764d3
anil843324 <anilkrsingh14200@gmail.com>
2022-10-04 15:12:08 +0530

    single feild search done

--- OwnComplete-node-bootcamp/code-step-by-step-backend/searchApi/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/searchApi/index.js
@@ -6,7 +6,22 @@ const product = require("./products");
 
 const app = express();
 
+app.get("/search/:key", async (req,res)=>{
+ 
+   console.log( req.params.key);
+   
+  let data= await product.find(
+ 
+    {
+      "$or":[
 
+        { "name":{$regex:req.params.key}}
+      ]
+    }
+
+  );
+  res.send(data);
+})
 
 app.listen(8000, () => {
   console.log(`port is listing ${8000}...`);
