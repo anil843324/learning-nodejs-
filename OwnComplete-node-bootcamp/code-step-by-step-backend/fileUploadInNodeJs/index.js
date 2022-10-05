094afc7bf90189a1eb3d762ba5643a9968ea44d6
anil843324 <anilkrsingh14200@gmail.com>
2022-10-05 08:27:53 +0530

    now file uploaded 👍😁

--- OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 
-require("./config");
-
-const product = require("./products");
+const multer = require("multer");
 
 const app = express();
 
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    }
+  })
+}).single("user_file")
 
+app.post("/upload", upload, (req, res) => {
+  res.send("file upload");
+});
 
 app.listen(8000, () => {
   console.log(`port is listing ${8000}...`);
