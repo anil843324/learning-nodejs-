e2fb645a2cf89409dc5dbd4cadbfbff8f61f8999
anil843324 <anilkrsingh14200@gmail.com>
2022-10-05 08:47:14 +0530

    os module done

--- OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
@@ -1,24 +1,55 @@
-const express = require("express");
 
-const multer = require("multer");
 
-const app = express();
+const os=require('os');
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
-    }
-  })
-}).single("user_file")
 
-app.post("/upload", upload, (req, res) => {
-  res.send("file upload");
-});
+// console.log( os.arch())
 
-app.listen(8000, () => {
-  console.log(`port is listing ${8000}...`);
-});
+// console.log( os.freemem()/(1024*1024*1024)  );
+
+
+// console.log( os.totalmem()/(1024*1024*1024)  );
+
+// console.log(os.hostname())
+// console.log(os.platform());
+
+ console.log( os.userInfo())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+
+// const multer = require("multer");
+
+// const app = express();
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "uploads");
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+//     }
+//   })
+// }).single("user_file")
+
+// app.post("/upload", upload, (req, res) => {
+//   res.send("file upload");
+// });
+
+// app.listen(8000, () => {
+//   console.log(`port is listing ${8000}...`);
+// });
