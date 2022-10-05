9f461d3e31edc5f1bb4cecb375447fff5d177131
anil843324 <anilkrsingh14200@gmail.com>
2022-10-05 09:10:10 +0530

    event emiter done

--- OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
+++ OwnComplete-node-bootcamp/code-step-by-step-backend/fileUploadInNodeJs/index.js
@@ -1,24 +1,49 @@
 
+// Events Emitter
 
-const os=require('os');
+const express=require('express')
 
+const app=express();
 
-// console.log( os.arch())
+const EventEmitter=require('events');
 
-// console.log( os.freemem()/(1024*1024*1024)  );
+const event= new EventEmitter();
 
+let count=0;
+event.on("countAPI",()=>{
 
-// console.log( os.totalmem()/(1024*1024*1024)  );
-
-// console.log(os.hostname())
-// console.log(os.platform());
-
- console.log( os.userInfo())
-
-
-
+   count++;
+   console.log( "event Called",count);
+})
 
 
+app.get("/",(req,res)=>{
+
+  res.send('api called')
+  event.emit("countAPI")
+})
+
+app.get("/search",(req,res)=>{
+
+  res.send(' search api called')
+
+  event.emit("countAPI")
+})
+
+app.get("/update",(req,res)=>{
+
+  res.send(' update api called')
+
+  event.emit("countAPI")
+
+})
+
+
+
+app.listen(8000,()=>{
+
+   console.log(`serve listen on ${8000}`)
+})
 
 
 
@@ -27,6 +52,38 @@ const os=require('os');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// os module
+
+// const os=require('os');
+
+// // console.log( os.arch())
+
+// // console.log( os.freemem()/(1024*1024*1024)  );
+
+// // console.log( os.totalmem()/(1024*1024*1024)  );
+
+// // console.log(os.hostname())
+// // console.log(os.platform());
+
+//  console.log( os.userInfo())
+
+
+///////////////////////
+
+///////////////////////////////
 
 
 // const express = require("express");
