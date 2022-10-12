cf31a70c6a156728e04df00158048e0a81565fdd
anil843324 <anilkrsingh14200@gmail.com>
2022-10-12 18:13:15 +0530

    curd with express done

--- OwnComplete-node-bootcamp/4-natours/starter/app.js
+++ OwnComplete-node-bootcamp/4-natours/starter/app.js
@@ -1,17 +1,117 @@
-import express from 'express';
+const express = require('express');
+
+const fs = require('fs');
+const { send } = require('process');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'hello from the server side!', app: 'Natours' });
+// middleware
+app.use(express.json());
+
+// v1= verson of api
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'suceess',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/' , (req,res)=>{
+// get data through id
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id * 1;
 
-     res.send('you can post to this endpoint...')
-} )
+  if (!(id > tours.length)) {
+    const tour = tours.find((el) => el.id === id);
+
+    res.status(200).json({
+      status: 'suceess',
+      data: {
+        tour,
+      },
+    });
+  } else {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+});
+
+// post request
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
+
+
+
+// updated methcod but actually not work
+
+app.patch('/api/v1/tours/:id', (req,res)=>{
+
+    if(req.params.id*1 > tours.length){
+        return res.status(404).json({
+          status:'fail',
+          message:'Invalid Id'
+        })
+    }
+
+     res.status(200).json({
+       status:'success',
+       data:{
+         tour:'<Updated tour here....>'
+       }
+     })
+
+
+})
+
+// delete method
+
+app.delete('/api/v1/tours/:id', (req,res)=>{
+
+  if(req.params.id*1 > tours.length){
+      return res.status(404).json({
+        status:'fail',
+        message:'Invalid Id'
+      })
+  }
+
+   res.status(204).json({
+     status:'success',
+     data:null
+   })
+
+
+})
+
+
+
 
 
 
