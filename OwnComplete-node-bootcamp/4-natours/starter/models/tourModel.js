6cfdf1b2e491dad6291fcfb3daa1402ad797a020
anil843324 <anilkrsingh14200@gmail.com>
2022-10-16 22:52:51 +0530

    tour model finally created

--- OwnComplete-node-bootcamp/4-natours/starter/models/tourModel.js
+++ OwnComplete-node-bootcamp/4-natours/starter/models/tourModel.js
@@ -1,19 +1,68 @@
-
- const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
     required: [true, 'A tour must have a name'],
+    trim: true,
   },
-  rating: {
+
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
-module.exports=Tour;
+module.exports = Tour;
