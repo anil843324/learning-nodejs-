8b764c4060b1e0a8d36a96baf0e68f9fa2bd239a
anil843324 <anilkrsingh14200@gmail.com>
2022-10-17 09:34:45 +0530

    data successfully uploaded from  json file

---
+++ OwnComplete-node-bootcamp/4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // warning added
+  })
+  .then(() => console.log('DB connection successful'));
+
+// read json file
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// import data in to database
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+
+    console.log('data successfully loaded!');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete all data from database
+const deleteData = async () => {
+    try {
+      await  Tour.deleteMany();
+  
+      console.log('data successfully deleted!');
+       process.exit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// if(process.arg[2] === '--delete'){
+//     deleteData();
+// }else if(process.arg[2] === '--import'){
+//   importData()
+// }
+
+
+
+// //  console.log( process.argv[2])  
+
+importData()
--- OwnComplete-node-bootcamp/4-natours/starter/server.js
+++ OwnComplete-node-bootcamp/4-natours/starter/server.js
@@ -1,36 +1,27 @@
+const mongoose = require('mongoose');
 
- const mongoose=require('mongoose');
+const dotenv = require('dotenv');
 
-const dotenv=require('dotenv');
+dotenv.config({ path: './config.env' });
 
-dotenv.config({path:'./config.env'});
+const app = require('./app');
 
-const app=require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-const DB=process.env.DATABASE.replace('<PASSWORD>',  process.env.DATABASE_PASSWORD);
-
-
-mongoose.connect(DB,{
-  useNewUrlParser:true,
-  useCreateIndex:true,
-  useFindAndModify:false,
-  useUnifiedTopology: true // warning added
-
-}).then( ()=> console.log('DB connection successful'))
-
-
-
-
-
-
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // warning added
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 5000;
 
-
-
 app.listen(port, () => {
-
   console.log(`app running on port ${port}...`);
 });
-
-
