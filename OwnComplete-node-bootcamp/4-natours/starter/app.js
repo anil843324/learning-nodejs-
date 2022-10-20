ae164f6d3fdf717f8d1041c8460cf102fc962b4a
anil843324 <anilkrsingh14200@gmail.com>
2022-10-20 10:35:57 +0530

    implemented global error handing

--- OwnComplete-node-bootcamp/4-natours/starter/app.js
+++ OwnComplete-node-bootcamp/4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const  AppError=require('./utils/appError')
+const globalErrorHandler=require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 
 const userRouter = require('./routes/userRoutes');
@@ -10,25 +12,15 @@ const app = express();
 
 // 1. middleware
 
-if(process.env.NODE_ENV=== 'development'){
-  
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-
 app.use(express.json());
 
-app.use( express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 // v1= verson of api
-
-// Refectring of route
-
-app.use((req, res, next) => {
-  console.log('hello from middleware');
-
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -40,5 +32,14 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 
 app.use('/api/v1/users', userRouter);
+
+// handle non match route
+
+app.all('*', (req, res, next) => {
+
+ next( new AppError(`Can't find ${req.originalUrl} on this server`,404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
