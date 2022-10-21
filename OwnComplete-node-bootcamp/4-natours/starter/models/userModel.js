73dfbc71d1eaaeef753972552c70b07aec8cd820
anil843324 <anilkrsingh14200@gmail.com>
2022-10-21 11:49:20 +0530

    maganing password ecrypted done

---
+++ OwnComplete-node-bootcamp/4-natours/starter/models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt=require('bcryptjs')
+
+// name  email photo password , passwordConform
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'please provide a passwrod'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate:{
+      // this only work on  CREATE AND SAVE !!!
+      validator:function(el){
+
+          return el===this.password 
+      },
+      message:'Passwords are not the same!'
+    }
+  },
+});
+
+userSchema.pre('save', async function(next){
+
+  // only run this fucntion if password was actually modified
+  if(!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+   this.password= await  bcrypt.hash(this.password,12)
+
+
+   // dlete passwordConfirm field
+   this.passwordConfirm= undefined;
+
+   next();
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- OwnComplete-node-bootcamp/4-natours/starter/routes/userRoutes.js
+++ OwnComplete-node-bootcamp/4-natours/starter/routes/userRoutes.js
@@ -1,18 +1,23 @@
 const express = require('express');
 
-const userController = require('../controllers/userController');
+// const userController = require('../controllers/userController');
+
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
 
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+ router.post('/signup',authController.signup);
+
+// router
+//   .route('/')
+//   .get(userController.getAllUsers);
+//   .post(userController.createUser);
+
+// router
+//   .route('/:id')
+//   .get(userController.getUser);
+//   .patch(userController.updateUser);
+//   .delete(userController.deleteUser);
 
 module.exports = router;
