7cf16db16a3eaa7dd5e2a1b34b7303ad70cceb6a
anil843324 <anilkrsingh14200@gmail.com>
2022-10-26 22:22:18 +0530

    Loged in  user can  update email and name

--- OwnComplete-node-bootcamp/4-natours/starter/controllers/userController.js
+++ OwnComplete-node-bootcamp/4-natours/starter/controllers/userController.js
@@ -1,11 +1,20 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
 
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
 
-exports.getAllUsers =  catchAsync(async (req, res,next) => {
-  const users = await  User.find();
+  return newObj;
+};
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
 
   // Send response
   res.status(200).json({
@@ -14,6 +23,35 @@ exports.getAllUsers =  catchAsync(async (req, res,next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1 Create error if user post password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates . Please use / updateMyPassword. ',
+        400
+      )
+    );
+  }
+
+  
+// 2 Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+// 3 update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data:{
+     user: updatedUser
+    }
   });
 });
 
