2e68867ec74828e8427363f66590786b33dc83c5
anil843324 <anilkrsingh14200@gmail.com>
2022-10-26 19:34:06 +0530

    Done Current User Password Update

--- OwnComplete-node-bootcamp/4-natours/starter/controllers/authController.js
+++ OwnComplete-node-bootcamp/4-natours/starter/controllers/authController.js
@@ -13,6 +13,20 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+  const createSendToken=(user,statusCode,res)=>{
+
+    const token = signToken(user._id);
+
+    res.status(statusCode).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      },
+    });
+
+  }
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -23,15 +37,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-  const token = signToken(newUser._id);
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken( newUser,201,res)
+
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -53,12 +61,8 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3 If everyThing ok , send token to client
 
-  const token = signToken(user._id);
+  createSendToken( user,200,res)
 
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -189,13 +193,40 @@ exports.resetPassword = catchAsync( async (req, res, next) => {
 
   // 4 Long the user in , sned JWT
 
-  const token = signToken(user._id);
-
-  res.status(200).json({
-    status: 'success',
-    token
-  });
-
-
+  createSendToken( user,200,res)
 
 }) ;
+
+
+
+exports.updatePassword= catchAsync( async (req,res,next)=>{
+
+
+  // 1 Get user from collection
+ 
+   const user=await User.findById(req.user.id).select('+password');
+
+
+  //2 check if posted current password is correct
+
+   if(! (await user.correctPassword(req.body.passwordCurrent,user.password))){
+
+     return next( new AppError('Your corrent password is worng', 401))
+   }
+  // 3  if so , update password
+   
+   user.password=req.body.password;
+   user.passwordConfirm=req.body.passwordConfirm;
+   await user.save();
+   // User.findByIdUpdate will NOt work as intended!
+
+  // 4  log user in , send jwt
+
+  createSendToken( user,200,res)
+
+
+
+});
+
+
+
--- OwnComplete-node-bootcamp/4-natours/starter/routes/userRoutes.js
+++ OwnComplete-node-bootcamp/4-natours/starter/routes/userRoutes.js
@@ -1,21 +1,23 @@
 const express = require('express');
 
- const userController = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
- router.post('/signup',authController.signup);
- router.post('/login',authController.login);
- router.post('/forgotPassword',authController.forgotPassword);
- router.patch('/resetPassword/:token',authController.resetPassword);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
 
-
-router
-  .route('/')
-  .get(userController.getAllUsers);
+router.route('/').get(userController.getAllUsers);
 //   .post(userController.createUser);
 
 // router
