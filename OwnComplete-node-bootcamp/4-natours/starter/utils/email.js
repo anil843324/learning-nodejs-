ee631c04d762ab943cd4f654f680d4ff747ae573
anil843324 <anilkrsingh14200@gmail.com>
2022-10-26 15:22:07 +0530

    email with some problem

---
+++ OwnComplete-node-bootcamp/4-natours/starter/utils/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1 create a transpoter
+  const transpoter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    // Active in gamil 'less secure app" option
+  });
+
+  // 2 Defnie the email options
+  const mailOptions = {
+    from: 'anil kumar <hello@anil.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    //  html:
+  };
+
+  // 3 Actually send the email
+  await transpoter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
