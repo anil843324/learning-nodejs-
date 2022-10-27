2a1ee787897e668671b2eeda2c2a938d97c3bd85
anil843324 <anilkrsingh14200@gmail.com>
2022-10-27 09:45:55 +0530

    little  cookie implemented

--- OwnComplete-node-bootcamp/4-natours/starter/controllers/authController.js
+++ OwnComplete-node-bootcamp/4-natours/starter/controllers/authController.js
@@ -13,19 +13,32 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-  const createSendToken=(user,statusCode,res)=>{
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
 
-    const token = signToken(user._id);
+  const cookieIptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
 
-    res.status(statusCode).json({
-      status: 'success',
-      token,
-      data: {
-        user
-      },
-    });
+   if(process.env.NODE_ENV === 'production ') cookieIptions.secure=true;
 
-  }
+
+  res.cookie('jwt', token, cookieIptions);
+
+  // Remove password  from output
+   user.password= undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -37,9 +50,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-
-  createSendToken( newUser,201,res)
-
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -61,8 +72,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3 If everyThing ok , send token to client
 
-  createSendToken( user,200,res)
-
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -168,65 +178,53 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = catchAsync( async (req, res, next) => {
+exports.resetPassword = catchAsync(async (req, res, next) => {
   // 1 get user based on the token
   const hashedToken = crypto
     .createHash('sha256')
     .update(req.params.token)
     .digest('hex');
 
-     const user= await User.findOne({passwordResetToken:hashedToken ,passwordResetExpires:{$gt: Date.now()} })
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
   //2  If thokenhas not expired , and there is user, set the new password
 
-   if(!user){
-     return next( new AppError('Token is invalid or has expired',400))
-   }
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
 
-    user.password=req.body.password;
-    user.passwordConfirm=req.body.passwordConfirm;
-    user.passwordResetToken=undefined;
-    user.passwordResetExpires=undefined;
-    await user.save();
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
   // 3 Udpate changePasswordAt property for the user
-
-
 
   // 4 Long the user in , sned JWT
 
-  createSendToken( user,200,res)
+  createSendToken(user, 200, res);
+});
 
-}) ;
-
-
-
-exports.updatePassword= catchAsync( async (req,res,next)=>{
-
-
+exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1 Get user from collection
- 
-   const user=await User.findById(req.user.id).select('+password');
 
+  const user = await User.findById(req.user.id).select('+password');
 
   //2 check if posted current password is correct
 
-   if(! (await user.correctPassword(req.body.passwordCurrent,user.password))){
-
-     return next( new AppError('Your corrent password is worng', 401))
-   }
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your corrent password is worng', 401));
+  }
   // 3  if so , update password
-   
-   user.password=req.body.password;
-   user.passwordConfirm=req.body.passwordConfirm;
-   await user.save();
-   // User.findByIdUpdate will NOt work as intended!
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdUpdate will NOt work as intended!
 
   // 4  log user in , send jwt
 
-  createSendToken( user,200,res)
-
-
-
+  createSendToken(user, 200, res);
 });
-
-
-
