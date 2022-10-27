d12c8f23e026815cdf4390e0eafc0e6df25081af
anil843324 <anilkrsingh14200@gmail.com>
2022-10-27 12:32:46 +0530

    Authentication and Authorization and Security done

--- OwnComplete-node-bootcamp/4-natours/starter/app.js
+++ OwnComplete-node-bootcamp/4-natours/starter/app.js
@@ -1,27 +1,65 @@
 const express = require('express');
-
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp=require('hpp')
 
-const  AppError=require('./utils/appError')
-const globalErrorHandler=require('./controllers/errorController')
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1. middleware
+// 1. Global  middleware
 
+// Set security HTTP  headers
+app.use(helmet());
+
+// Development loging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//  limit request  from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
+// Body  parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+
+app.use(xss());
+
+// Prevent parameter pollutin
+ app.use(hpp({
+  whitelist:[
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+ }));
+
+// Serving  static  files
 app.use(express.static(`${__dirname}/public`));
 
-// v1= verson of api
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -36,8 +74,7 @@ app.use('/api/v1/users', userRouter);
 // handle non match route
 
 app.all('*', (req, res, next) => {
-
- next( new AppError(`Can't find ${req.originalUrl} on this server`,404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 app.use(globalErrorHandler);
